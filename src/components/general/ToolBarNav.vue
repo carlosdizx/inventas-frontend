c2f17a0b3840e12aff0dfa2ef9d2bcba1b6b12ca
carlosdizx <carlodiaz@umariana.edu.co>
2021-07-17 12:52:49 -0500

    Creado componente generico para navegacion

---
+++ src/components/general/ToolBarNav.vue
@@ -0,0 +1,27 @@
+<template>
+	<v-card>
+		<v-tabs
+			background-color="indigo"
+			center-active
+			dark
+			next-icon="mdi-arrow-right-bold-box-outline"
+			prev-icon="mdi-arrow-left-bold-box-outline"
+			show-arrows
+		>
+			<router-link :to="tab.link" v-for="tab in tabs" v-slot="{Navigate}" custom>
+				<v-tab>
+					{{ tab.texto }}
+				</v-tab>
+			</router-link>
+		</v-tabs>
+	</v-card>
+</template>
+
+<script>
+	export default {
+		name: 'ToolBarNav',
+		props: {
+			tabs: null,
+		},
+	};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,12 @@
 <template>
 	<div>
+		<ToolBarNav :tabs="tabs" />
 		<router-link to="inicio" v-slot="{ navigate }" custom>
-			<v-btn @click="navigate">Login</v-btn>
+			<v-btn block @click="navigate">Login</v-btn>
 		</router-link>
 		<router-link to="registro" v-slot="{ navigate }" custom>
-			<v-btn @click="navigate">Registro</v-btn>
+			<v-btn block @click="navigate">Registro</v-btn>
 		</router-link>
-		<br />
-		<hr />
 		{{ usuario }}
 	</div>
 </template>
@@ -15,9 +14,16 @@
 <script>
 	import Vue from 'vue';
 	import { mapState, mapActions } from 'vuex';
+	import ToolBarNav from '../components/general/ToolBarNav';
 
 	export default Vue.extend({
 		name: 'Home',
+		components: {
+			ToolBarNav,
+		},
+		data: () => ({
+			tabs: [''],
+		}),
 		computed: {
 			...mapState(['usuario']),
 		},
@@ -25,6 +31,18 @@
 			...mapActions(['comprobarToken', 'cargarUsuario']),
 		},
 		async created() {
+			this.tabs = [
+				{ link: '/', texto: '1' },
+				{ link: '/', texto: '2' },
+				{ link: '/', texto: '3' },
+				{ link: '/', texto: '4' },
+				{ link: '/', texto: '5' },
+				{ link: '/', texto: '9' },
+				{ link: '/', texto: '10' },
+				{ link: '/', texto: '11' },
+				{ link: '/', texto: '12' },
+				{ link: '/', texto: '13' },
+			];
 			await this.cargarUsuario();
 			await this.comprobarToken();
 		},
