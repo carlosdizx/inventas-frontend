e2f1f00663c248e84a02ba63eed4ce34e82087cc
carlosdizx <carlodiaz@umariana.edu.co>
2021-07-18 14:14:56 -0500

    Construida tabla que de manera dinamica pobla filas y columnas

---
+++ src/components/general/Tabla.vue
@@ -0,0 +1,30 @@
+<template>
+	<v-simple-table>
+		<template v-slot:default>
+			<thead>
+				<tr>
+					<th class="text-left" v-for="columna in columnas" :key="columna">
+						{{ columna }}
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="item in filas">
+					<td v-for="valor in Object.values(item)">{{ valor }}</td>
+				</tr>
+			</tbody>
+		</template>
+	</v-simple-table>
+</template>
+
+<script>
+	export default {
+		name: 'TablaSimple',
+		props: {
+			columnas: Array(),
+			filas: Array(),
+		},
+	};
+</script>
+
+<style scoped></style>
--- src/views/Forms.vue
+++ src/views/Forms.vue
@@ -1,21 +1,33 @@
 <template>
-	<ToolBarForms :tabs="tabs"/>
+	<div>
+		<ToolBarForms :tabs="tabs" />
+		<Tabla :columnas="columnas" :filas="filas" />
+	</div>
 </template>
 
 <script>
 	import ToolBarForms from '../components/forms/ToolBarForms';
+	import Tabla from '../components/general/Tabla';
 	export default {
 		name: 'Forms',
-		data: () => ({
-			tabs: [''],
-		}),
 		components: {
 			ToolBarForms,
+			Tabla,
 		},
-    mounted() {
-      this.tabs = ['mdi-archive', 'mdi-account-outline'];
-    }
-  };
+		data: () => ({
+			tabs: ['mdi-archive', 'mdi-account-outline'],
+			columnas: ['id', 'nombres', 'apellidos', 'celular', 'correo'],
+			filas: [
+				{
+					id: 1,
+					nombres: 'Carlos Ernesto',
+					apellidos: 'Díaz Basante',
+					celular: 3163930876,
+					correo: 'carlodiaz@umariana.edu.co',
+				},
+			],
+		}),
+	};
 </script>
 
 <style scoped></style>
