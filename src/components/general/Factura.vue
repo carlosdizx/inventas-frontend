41d5044a82e49cfcfd0b46e17324cd537e92931e
carlosdizx <carlodiaz@umariana.edu.co>
2021-07-21 18:14:46 -0500

    Se desacoplo logica y componentes de la vista de listado de facturas

--- src/components/general/Factura.vue
+++ src/components/general/Factura.vue
@@ -1,54 +1,121 @@
 <template>
-	<v-container>
-		<v-card class="ticket">
-			<v-card-title>Factura #{{ this.facturasSeleccionada[0].id }}</v-card-title>
+	<div>
+		<v-card class="mx-auto" max-width="700">
+			<v-card-title>Listado de Facturas</v-card-title>
 			<v-card-subtitle>
-				Total <strong>{{ total | toUSD }}</strong>
-				<br />
-				Fecha
-				<strong>{{ this.facturasSeleccionada[0].fecha }}</strong>
-				<br />
-				Cliente:<strong> {{ documento }}</strong>
+				<v-row>
+					<v-col cols="5">
+						<v-select
+							v-model.number="id"
+							label="Seleccione una factura"
+							:items="facturas"
+							item-text="id"
+						/>
+					</v-col>
+					<v-col cols="6">
+						<v-btn block :disabled="!id" color="success" @click="buscarFactura">
+							Buscar factura
+						</v-btn>
+					</v-col>
+				</v-row>
 			</v-card-subtitle>
-			<v-card-text>
-				<Tabla :columnas="columnas" :filas="Object.values(facturasSeleccionada)" />
-			</v-card-text>
-			<v-card-actions> </v-card-actions>
+			<v-card v-if="id !== 0 && mostrar">
+				<v-card-title>Factura #{{ idSeleccionado }}</v-card-title>
+			</v-card>
 		</v-card>
-	</v-container>
+		<!--
+		<v-container>
+			<v-card class="ticket" v-if="idSeleccionado !== null">
+				<v-card-title>Factura #{{ idSeleccionado }}</v-card-title>
+				<v-card-subtitle>
+					Total <strong>{{ total | toUSD }}</strong>
+					<br />
+					Fecha
+					<strong>{{ this.facturasSeleccionada[0].fecha }}</strong>
+					<br />
+					Cliente:<strong> {{ documento }}</strong>
+				</v-card-subtitle>
+				<v-card-text>
+					<Tabla :columnas="columnas" :filas="Object.values(facturasSeleccionada)" />
+				</v-card-text>
+				<v-card-actions> </v-card-actions>
+			</v-card>
+		</v-container>
+		-->
+	</div>
 </template>
 
 <script>
 	import Tabla from './Tabla';
+	import { mapActions } from 'vuex';
+	import Swal from 'sweetalert2';
 	export default {
 		name: 'Factura',
 		components: { Tabla },
 		data: () => ({
 			id: 0,
+			idSeleccionado: 0,
 			fecha: '',
 			documento: '',
 			total: 0,
+			facturas: [],
+			infoFacturas: [],
 			columnas: [],
+			mostrar: false,
+			items: [],
 		}),
-		props: {
-			facturasSeleccionada: Array(),
-		},
 		methods: {
+			...mapActions(['listarFacturas', 'listarInfoFacturas']),
 			actulizarInfo() {
-				if (this.facturasSeleccionada.length > 0) {
-					this.columnas = Object.keys(this.facturasSeleccionada[0]);
-					this.id = this.facturasSeleccionada[0].id;
-					this.fecha = this.facturasSeleccionada[0].fecha;
-					this.documento = this.facturasSeleccionada[0].descripcion;
+				if (this.items.length > 0) {
+					this.columnas = Object.keys(this.items[0]);
+					this.id = this.items[0].id;
+					this.fecha = this.items[0].fecha;
+					this.documento = this.items[0].descripcion;
 					this.total = 0;
-					this.facturasSeleccionada.forEach((valor) => {
+					this.items.forEach((valor) => {
 						this.total += valor.subTotal;
 					});
 				}
 			},
+			async cargarFacturas() {
+				const respuesta = await this.listarFacturas();
+				if (typeof respuesta.data.Mensaje === 'string') {
+					return;
+				}
+				this.facturas = respuesta.data.Mensaje;
+			},
+			async cargarInfoFacturas() {
+				const respuesta = await this.listarInfoFacturas();
+				if (typeof respuesta.data.Mensaje === 'string') {
+					return;
+				}
+				this.infoFacturas = respuesta.data.Mensaje;
+				this.columnas = Object.keys(this.infoFacturas[0]);
+			},
+			async buscarFactura() {
+				this.idSeleccionado = this.id;
+				this.items = [];
+				await this.infoFacturas.forEach((info) => {
+					if (info.id === this.id) {
+						this.items.push(info);
+					}
+				});
+				this.mostrar = true;
+			},
 		},
 		async mounted() {
-			this.actulizarInfo();
+			try {
+				await this.cargarFacturas();
+				await this.cargarInfoFacturas();
+				this.actulizarInfo();
+			} catch (e) {
+				await Swal.fire(
+					'No se pudo obtener las facturas',
+					'No se te concedio acceso',
+					'warning'
+				);
+			}
 		},
 	};
 </script>
--- src/views/ListadosFacturas.vue
+++ src/views/ListadosFacturas.vue
@@ -1,41 +1,15 @@
 <template>
 	<v-container>
-		<v-card class="mx-auto" max-width="700">
-			<v-card-title>Listado de Facturas</v-card-title>
-			<div v-if="!mostrar">No hay facturas</div>
-			<v-card-subtitle>
-				<v-row>
-					<v-col cols="6">
-						<v-select
-							v-model.number="idSeleccionado"
-							label="Seleccione una factura"
-							:items="facturas"
-							item-text="id"
-						/>
-					</v-col>
-					<v-col>
-						<v-btn :disabled="!idSeleccionado" color="success" @click="buscarFactura">
-							Buscar
-						</v-btn>
-					</v-col>
-				</v-row>
-			</v-card-subtitle>
-			<div v-if="mostrar && buscado">
-				<Factura :facturas-seleccionada="facturasSeleccionada" />
-			</div>
-		</v-card>
+    <Factura></Factura>
 	</v-container>
 </template>
 
 <script>
-	import { mapActions } from 'vuex';
-	import Swal from 'sweetalert2';
-	import Tabla from '../components/general/Tabla';
 	import Factura from '../components/general/Factura';
 
 	export default {
 		name: 'ListadosFacturas',
-		components: { Factura, Tabla },
+		components: { Factura },
 		data: () => ({
 			facturas: [],
 			infoFacturas: [],
@@ -45,47 +19,5 @@
 			facturasSeleccionada: [],
 			buscado: false,
 		}),
-		methods: {
-			...mapActions(['listarFacturas', 'listarInfoFacturas']),
-			async cargarFacturas() {
-				const respuesta = await this.listarFacturas();
-				if (typeof respuesta.data.Mensaje === 'string') {
-					return (this.mostrar = false);
-				}
-				this.facturas = respuesta.data.Mensaje;
-			},
-			async cargarInfoFacturas() {
-				const respuesta = await this.listarInfoFacturas();
-				if (typeof respuesta.data.Mensaje === 'string') {
-					return (this.mostrar = false);
-				}
-				this.infoFacturas = respuesta.data.Mensaje;
-				this.columnas = Object.keys(this.infoFacturas[0]);
-				this.mostrar = true;
-			},
-			async buscarFactura() {
-				this.facturasSeleccionada = [];
-				await this.infoFacturas.forEach((info) => {
-					if (info.id === this.idSeleccionado) {
-						this.facturasSeleccionada.push(info);
-					}
-				});
-				this.buscado = true;
-			},
-		},
-		async mounted() {
-			try {
-				await this.cargarFacturas();
-				await this.cargarInfoFacturas();
-			} catch (e) {
-				await Swal.fire(
-					'No se pudo obtener las facturas',
-					'No se te concedio acceso',
-					'warning'
-				);
-			}
-		},
 	};
 </script>
-
-<style scoped></style>
