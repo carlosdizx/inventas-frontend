c41aa1b74d6413c24b948d4f66ad227e3e4dd6ce
carlosdizx <carlodiaz@umariana.edu.co>
2021-07-23 11:15:39 -0500

    Creado el lector de codigos de barras

--- src/components/general/LectorBarras.vue
+++ src/components/general/LectorBarras.vue
@@ -1,13 +1,39 @@
 <template>
-
+	<div>
+		<div id="camara"></div>
+		<div id="resultado"></div>
+	</div>
 </template>
 
 <script>
-export default {
-  name: "LectorBarras"
-}
+	import Quagga from 'quagga';
+	export default {
+		name: 'LectorBarras',
+		mounted() {
+			Quagga.init(
+				{
+					inputStream: {
+						name: 'Live',
+						type: 'LiveStream',
+						target: document.querySelector('#camara'), // Or '#yourElement' (optional)
+					},
+					decoder: {
+						readers: ['code_128_reader'],
+					},
+				},
+				function(err) {
+					if (err) {
+						console.log(err);
+						return;
+					}
+					console.log('Initialization finished. Ready to start');
+					Quagga.start();
+				}
+			);
+			Quagga.onDetected(function(data) {
+				console.log(data.codeResult.code);
+				document.querySelector('#resultado').innerText = data.codeResult.code;
+			});
+		},
+	};
 </script>
-
-<style scoped>
-
-</style>
