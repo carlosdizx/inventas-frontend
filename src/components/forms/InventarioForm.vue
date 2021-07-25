4902948faf9f404f89ab58e38884c741cd65408f
carlosdizx <carlodiaz@umariana.edu.co>
2021-07-25 11:31:51 -0500

    Se le agregaron los tipos de formatos de lectura

--- src/components/forms/InventarioForm.vue
+++ src/components/forms/InventarioForm.vue
@@ -92,6 +92,7 @@
 		data: () => ({
 			display: false,
 			codigo: '',
+
 		}),
 		methods: {},
 	};
--- src/components/general/Quagga.vue
+++ src/components/general/Quagga.vue
@@ -1,5 +1,5 @@
 <template>
-	<v-quagga :onDetected="logIt" />
+	<v-quagga :onDetected="logIt" :readerSize="readerSize" :readerTypes="readerTypes" />
 </template>
 
 <script>
@@ -11,19 +11,33 @@
 
 	export default {
 		name: 'Quagga',
-		data() {
-			return {
-				detecteds: [],
-			};
-		},
+		data: () => ({
+			readerSize: {
+				width: 500,
+				height: 480,
+			},
+			readerTypes: [
+				'ean_reader',
+				'code_128_reader',
+				'ean_8_reader',
+				'code_39_reader',
+				'code_39_vin_reader',
+				'codabar_reader',
+				'upc_reader',
+				'upc_e_reader',
+				'2of5_reader',
+				'i2of5_reader',
+				'code_93_reader',
+			],
+		}),
 		methods: {
 			logIt(data) {
 				setTimeout(async function() {
 					await Swal.fire({
-            title: 'Codigo escaneado',
-            icon: 'success',
-            timer: 1000,
-          });
+						title: 'Codigo escaneado',
+						icon: 'success',
+						timer: 1000,
+					});
 				}, 1000);
 			},
 		},
